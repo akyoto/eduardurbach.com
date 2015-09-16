a596eb437fef2dc8bfd6fb6df7874b41cd4fc449
Eduard Urbach <e.urbach@gmail.com>
2015-09-16 19:52:17 +0900

    Fixed fonts and scripts

--- scripts/init.js
+++ scripts/init.js
@@ -2,31 +2,6 @@
 $(window).load(function() {
 	$(document).keydown(function(event) {
 		switch(event.which) {
-			// m
-			case 77:
-				aero.loadURL("/");
-				break;
-
-			// p
-			case 80:
-				aero.loadURL("/projects");
-				break;
-
-			// s
-			case 83:
-				aero.loadURL("/skills");
-				break;
-				
-			// h
-			case 72:
-				aero.loadURL("/hire");
-				break;
-
-			// w
-			case 87:
-				aero.loadURL("/web");
-				break;
-
 			// Left
 			case 37:
 				aero.navigateLeft();
