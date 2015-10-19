1d383a255a3fdbd9bb5cee20493b39965c4cd934
Eduard Urbach <e.urbach@gmail.com>
2015-10-20 00:45:31 +0900

    New content

--- scripts/init.js
+++ scripts/init.js
@@ -1,21 +1,28 @@
 // Load
 $(window).load(function() {
-	$(document).keydown(function(event) {
+	kaze.fadeSpeed = 300;
+
+	$(".navigation-item").each(function(index, item) {
+		var $item = $(item);
+
+		window.setTimeout(function() {
+			$item.addClass("navigation-item-ready");
+		}, (index + 1) * 200);
+	});
+
+	/*$(document).keydown(function(event) {
 		switch(event.which) {
 			// Left
 			case 37:
 				aero.navigateLeft();
 				break;
-
 			// Right
 			case 39:
 				aero.navigateRight();
 				break;
-
 			default:
 				return;
 		}
-
 		event.preventDefault();
-	});
+	});*/
 });
