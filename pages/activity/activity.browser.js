12b112b9ed3536760bed43d4d08cdd97befc3bdf
Eduard Urbach <e.urbach@gmail.com>
2015-11-14 20:59:59 +0900

    Changed margin-left animation to CSS transforms

--- pages/activity/activity.browser.js
+++ pages/activity/activity.browser.js
@@ -6,7 +6,7 @@ $.getJSON('https://api.github.com/users/blitzprog/events', function(data) {
 		})
 		.map(function(e) {
 			return e.payload.commits.map(function(commit) {
-				return '<li><a href="https://github.com/' + e.repo.name + '/commit/' + commit.sha + '" target="_blank">' + commit.message + '</a>'
+				return '<li class="commit"><a href="https://github.com/' + e.repo.name + '/commit/' + commit.sha + '" target="_blank">' + commit.message + '</a>'
 					+ '<span class="repository-name">' + e.repo.name.substring(e.repo.name.indexOf('/') + 1) + '</span>'
 					+ '</li>';
 			}).join('');
--- pages/other/other.js
+++ pages/other/other.js
@@ -3,10 +3,15 @@
 let fs = require('fs')
 let aero = require('aero')
 
+let excludePages = {
+	'me': true,
+	'api': true
+}
+
 module.exports = {
 	init: function() {
 		fs.readdir(aero.config.path.pages, (error, pages) => {
-			this.pages = pages.filter(page => page !== 'me' && !(page in aero.layout.json.nav))
+			this.pages = pages.filter(page => !(excludePages[page]) && !(page in aero.layout.json.nav))
 		})
 	},
 
