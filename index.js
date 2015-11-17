fda0ca2e84fad45ce7aef1da22429418f6f04af9
Eduard Urbach <e.urbach@gmail.com>
2015-11-17 23:34:44 +0900

    Using HTTPS with HTTP/2 and Let's Encrypt

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 
 let aero = require('aero')
 aero.run()
+
+let http = require('http')
+http.createServer(function(req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(1990);
