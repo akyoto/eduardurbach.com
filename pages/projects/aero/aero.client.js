5b15c385e762362786a81fc0699992adc7944d89
Eduard Urbach <e.urbach@gmail.com>
2016-04-13 01:10:11 +0900

    Updated projects

--- pages/projects/aero/aero.client.js
+++ pages/projects/aero/aero.client.js
@@ -1,6 +1,6 @@
 $.getJSON('https://api.github.com/repos/aerojs/aero/stargazers?clientid=e8fe5e8bcaf6b7ebe0534a93976dca8bdc320ee4&clientsecret=eae6fea79ebe2c919770e0c5e2e38d64d70453d5')
 .then(data => {
-	let stars = document.getElementById('aero-stars')
+	let stars = $('aero-stars')
 	stars.innerHTML = data.length
 	stars.classList.add('visible-text')
 })
