aa9a9de0addd13f1efa98cc39c86db587d2d5ab9
Eduard Urbach <e.urbach@gmail.com>
2016-09-26 22:00:26 +0900

    Only stop loading animation on load event

--- scripts/init.js
+++ scripts/init.js
@@ -1,14 +1,14 @@
-let navItems = document.querySelectorAll('.navigation-item');
+let navItems = document.querySelectorAll('.navigation-item')
 
 let fadeIndex = function(i) {
 	return function() {
-		navItems[i].classList.add('navigation-item-ready');
+		navItems[i].classList.add('navigation-item-ready')
 	}
 }
 
 for(let i = 0; i < navItems.length; i++) {
-	window.setTimeout(fadeIndex(i), i * 50);
+	window.setTimeout(fadeIndex(i), i * 50)
 }
 
 // Fade out loading animation
-$('loading-animation').classList.add('fade-out');
+window.onload = () => $('loading-animation').classList.add('fade-out')
