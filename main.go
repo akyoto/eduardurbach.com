fcd606d9042687a17af69eec7e0e0909233526bd
Eduard Urbach <e.urbach@gmail.com>
2018-05-10 11:06:54 +0900

    Started the Go rewrite

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/aerogo/aero"
+)
+
+func main() {
+	app := aero.New()
+	configure(app).Run()
+}
+
+func configure(app *aero.Application) *aero.Application {
+	app.Get("/", func(ctx *aero.Context) string {
+		return ctx.Text("Hello World")
+	})
+
+	return app
+}
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aerogo/aero"
+)
+
+func TestApp(t *testing.T) {
+	app := configure(aero.New())
+	println(app.Config.Title)
+}
