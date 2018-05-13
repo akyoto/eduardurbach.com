0c583c001a41c4b2050d4a925970e50fe018b46a
Eduard Urbach <e.urbach@gmail.com>
2018-05-13 09:49:35 +0900

    Added navigation icons

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+let navItems = document.getElementsByClassName("navigation-item")
+
+let fadeIndex = function(i) {
+	return function() {
+		navItems[i].classList.add("navigation-item-ready")
+	}
+}
+
+for(let i = 0; i < navItems.length; i++) {
+	window.setTimeout(fadeIndex(i), i * 50)
+}
+
+// Fade out loading animation
+window.onload = () => document.getElementById("loading").classList.add("fade-out")
